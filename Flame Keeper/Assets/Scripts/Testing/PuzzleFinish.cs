826f931f157cf3db4935a699e0313bc579549aa2
Lily <lilyhgxiao@gmail.com>
2019-02-20 21:34:52 -0500

    Created Puzzle 2 animations, timeline controller scripts and completion trigger scripts for puzzles

---
+++ Flame Keeper/Assets/Scripts/Testing/PuzzleFinish.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class PuzzleFinish : MonoBehaviour
+{
+    [Header("Events")]
+    public List<UnityEvent> eventsToPlay;
+
+    private bool played;
+
+    void Start()
+    {
+        played = false;
+    }
+
+    void Update()
+    {
+        if (CheckIfFinished() && !played)
+        {
+            OnFinish();
+            played = true;
+        }
+    }
+
+    protected void OnFinish()
+    {
+        foreach (UnityEvent events in eventsToPlay) 
+        {
+            if (events != null)
+            {
+                events.Invoke();
+            }
+        }
+        
+    }
+
+    protected abstract bool CheckIfFinished();
+}
---
+++ Flame Keeper/Assets/Scripts/Testing/PuzzleFinishOnPedestal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleFinishOnPedestal : PuzzleFinish
+{
+    [Header("Final Pedestal State")]
+    //make sure to input them in the same order
+    public List<Pedestal> pedestals;
+    public List<int> finalLevels; 
+
+    protected override bool CheckIfFinished()
+    {
+        int i;
+        for (i = 0; i < pedestals.Count; i++)
+        {
+            if (pedestals[i] == null || !IsLevelFinal(pedestals[i], finalLevels[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool IsLevelFinal(Pedestal pedestal, int finalLevel)
+    {
+        if (pedestal == null)
+        {
+            return false;
+        }
+        if (pedestal.GetCurrLevel() == finalLevel)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
