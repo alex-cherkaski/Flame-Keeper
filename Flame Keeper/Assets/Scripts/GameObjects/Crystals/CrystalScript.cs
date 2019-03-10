4a804b336460dc6465867f26c4f32aa9223d3307
cherkaski <alexcher81@gmail.com>
2019-03-10 16:25:03 -0400

    Replaced crystals in Tutorial and Level 1

--- Flame Keeper/Assets/Scripts/GameObjects/Crystals/CrystalScript.cs
+++ Flame Keeper/Assets/Scripts/GameObjects/Crystals/CrystalScript.cs
@@ -4,16 +4,25 @@ public class CrystalScript : MonoBehaviour
 {
     public float warmth = 10;
     public float rotateSpeed = 1.0f;
+    public ParticleSystem particles;
 
     // Update is called once per frame
     void Update()
     {
         float angle = 45 * Time.deltaTime * rotateSpeed;
-        this.transform.Rotate(angle, angle, angle, Space.Self);
+        this.transform.Rotate(0, angle, 0, Space.Self);
     }
 
     public float GetWarmth()
     {
         return warmth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            particles.Stop();
+        }   
     }
 }
