8c4452207cf4a0d60ee2ac9a058d29ebf9a4c070
cherkaski <alexcher81@gmail.com>
2019-03-24 17:56:21 -0400

    Added wires and script to tutorial level

---
+++ Flame Keeper/Assets/WireScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireScript : ActivatableObject
+{
+    public Color activatedColor;
+    public Color deactivatedColor;
+    public Pedestal pedestal;
+
+    private Color currentColor;
+    private MeshRenderer meshRenderer;
+
+    protected override void OnDepowered(Pedestal pedestal, int newLevel)
+    {
+        currentColor = deactivatedColor;
+    }
+
+    protected override void OnPowered(Pedestal pedestal, int newLevel)
+    {
+        currentColor = activatedColor;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        if (pedestal.GetCurrLevel() > 0)
+        {
+            meshRenderer.material.color = activatedColor;
+            currentColor = activatedColor;
+        }
+        else
+        {
+            meshRenderer.material.color = deactivatedColor;
+            currentColor = deactivatedColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.color = Color.Lerp(meshRenderer.material.color, currentColor, Time.deltaTime);
+    }
+}
