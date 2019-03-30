c54a25b2ad442a4f1548640743530aa6a2c4947e
Lily <lilyhgxiao@gmail.com>
2019-03-30 19:41:27 -0400

    Altered tutorial lightup cubes, added cutscenes to level 2, changed totem colours

---
+++ Flame Keeper/Assets/Scripts/Animations/PuzzleFinishOnCrystal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleFinishOnCrystal : PuzzleFinish
+{
+    [Header("Final Pedestal State")]
+    //make sure to input them in the same order
+    public GameObject crystal;
+
+    protected override bool CheckIfFinished()
+    {
+        if (crystal.activeSelf == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
